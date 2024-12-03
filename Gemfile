d75b901e921911c22c057e6487591695d8be534f
Thomas Powell <thomas.powell@progress.com>
2024-12-03 11:31:55 -0500

    Get pipelines and cache them
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source "https://rubygems.org"
 
-gem "buildkiterb", "~> 1.0.1"
+gem "buildkiterb", github: "disapprovaldog/buildkiterb", branch: "passparams"
 gem "faraday"
 gem "standard"
--- get_statuses.rb
+++ get_statuses.rb
@@ -1,11 +1,42 @@
 require "buildkite"
+require "yaml"
+require "date"
+require "fileutils"
 
 eval(File.read(".env"))
+
 Buildkite.configure do |config|
   config.token = BUILDKITE_AGENT_TOKEN
   config.org = BUILDKITE_ORG
 end
 
-Buildkite::Pipeline.list.data.each do |pipeline|
-  puts "Pipeline: #{pipeline.name}"
+pipeline_cache_files=Dir["pipelines*.yml"]
+
+pipelines_cache_files.grep_v("pipelines-#{Date.today.to_s}.yml").each do |filename|
+  FileUtils.rm_rf filename
 end
+
+pipeline_cache=pipeline_cache_files.grep("pipelines-#{Date.today.to_s}.yml")[0]
+
+pipelines=[]
+if pipeline_cache
+  pipelines = YAML.parse(File.read(pipeline_cache)).to_ruby
+else
+  page=1
+  loop do
+    list=Buildkite::Pipeline.list(page: page)
+
+    break unless list.total > 0
+
+    puts "<<< #{page} >>>"
+    list.data.each do |pipeline|
+      pipelines << pipeline
+    end
+    page += 1
+  end
+
+  File.open("pipelines.yml", "wt") do |f|
+    f.write(pipelines.to_yaml)
+  end
+end
+
